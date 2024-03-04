dbd14fccbfa02fb1d273bd8cb9dd1140a824fe90
zabokaa <sab_fem@gmx.de>
2024-03-04 18:07:29 +0100

    add async function for status of the API

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,20 @@ const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+// async function to display the status of the API    
+async function getStatus(e) {
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    // getting data back
+    const data = await response.json();
+
+    
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+
+}
