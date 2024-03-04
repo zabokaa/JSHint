27c75ca48645a53613d8083a04dc3bea9bbcd2e1
zabokaa <sab_fem@gmx.de>
2024-03-04 18:01:59 +0100

    create const in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const API_KEY = "YKC93GCPS3gNdG00NOUU2AWHNAg";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
