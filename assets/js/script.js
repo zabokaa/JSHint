1b0b82f7fcd4c77c0cf21d098d6f9783fdb0e7a4
zabokaa <sab_fem@gmx.de>
2024-03-04 18:03:17 +0100

    add eventListener script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
 const API_KEY = "YKC93GCPS3gNdG00NOUU2AWHNAg";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
