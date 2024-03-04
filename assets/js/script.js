c213f8d159753f02f8a6fb49c5dc583114f25fe6
zabokaa <sab_fem@gmx.de>
2024-03-04 18:37:55 +0100

    create displayStatus FUNC

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,6 @@ document.getElementById("status").addEventListener("click", e => getStatus(e));
 
 // async function to display the status of the API    
 async function getStatus(e) {
-
     const queryString = `${API_URL}?api_key=${API_KEY}`;
 
     const response = await fetch(queryString);
@@ -20,5 +19,15 @@ async function getStatus(e) {
     } else {
         throw new Error(data.error);
     }
+}
 
+// func to display the status of the API
+function displayStatus(data) {
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
 }
