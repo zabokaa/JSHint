6de8b1ae93c5ad7c8f02566f32898d0ed1467882
zabokaa <sab_fem@gmx.de>
2024-03-05 16:36:13 +0100

    debug: statusError + displayErrors FUNCs have not been called !

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 const API_KEY = "YKC93GCPS3gNdG00NOUU2AWHNAg";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
-const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+let resultsModal;
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+});
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
@@ -21,13 +26,12 @@ async function postForm(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data);
+        displayErrors(data);
     } else {
         throw new Error(data.error);
     }
 
 }
-
 
 
 // async function to display the status of the API    
@@ -41,10 +45,33 @@ async function getStatus(e) {
 
 
     if (response.ok) {
-        console.log(data);
+        displayStatus(data);
     } else {
         throw new Error(data.error);
     }
+}
+
+// func to display the errors
+function displayErrors(data) {
+    console.log('displayErrors called with data:', data);
+//  set content for the modal
+    let results = "";
+
+    let heading = `JSHint Results for ${data.file}`;
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}:</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+// set heading, results and show the modal
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
 }
 
 // func to display the status of the API
